6f78ab15017653b86ffa540d3ed1fd73bf4cee94
Komiljon <komiljonnajmitdinov99@gmail.com>
2022-02-23 11:09:05 +0500

    calc ok

--- js/app.js
+++ js/app.js
@@ -41,295 +41,451 @@
 
 
 
-function calculator(str) {
-     //Convert
-     let a,
-       b,
-       sum,
-       k,
-       romeN = ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX', 'X', ''],
-       operators = ['+', '-', '*', '/'],
-       j = 0;
+// function calculator(str) {
+//      //Convert
+//      let a,
+//        b,
+//        sum,
+//        k,
+//        romeN = ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX', 'X', ''],
+//        operators = ['+', '-', '*', '/'],
+//        j = 0;
    
-     str = str.split(' ');
+//      str = str.split(' ');
    
-     // Проверяем на ошибки
-     if (str.length == 1) throw new Error();
-     if (str.length > 3) throw new Error();
+//      // Проверяем на ошибки
+//      if (str.length == 1) throw new Error();
+//      if (str.length > 3) throw new Error();
    
-     //Определяем опрератор
-     operators.map((num) => {
-       if (num == str[1]) operators = num;
-     });
+//      //Определяем опрератор
+//      operators.map((num) => {
+//        if (num == str[1]) operators = num;
+//      });
    
-     b = str[str.length - 1];
-     a = str[0];
+//      b = str[str.length - 1];
+//      a = str[0];
    
-     // Проверяем на похожие Системы счисления
-     if (
-       (romeN.includes(a) && !romeN.includes(b)) 
-       (!romeN.includes(a) && romeN.includes(b))
-     ) {
-       throw new Error();
-     }
+//      // Проверяем на похожие Системы счисления
+//      if (
+//        (romeN.includes(a) && !romeN.includes(b)) 
+//        (!romeN.includes(a) && romeN.includes(b))
+//      ) {
+//        throw new Error();
+//      }
    
-     // Конвертируем Римские числа в Латинскую систему счисления
-     romeN.map((num, i) => {
-       if (a == num) {
-         a = i + 1;
-         j++;
-       }
+//      // Конвертируем Римские числа в Латинскую систему счисления
+//      romeN.map((num, i) => {
+//        if (a == num) {
+//          a = i + 1;
+//          j++;
+//        }
    
-       if (b == num) {
-         b = i + 1;
-         j++;
-       }
-     });
+//        if (b == num) {
+//          b = i + 1;
+//          j++;
+//        }
+//      });
    
-     a = Number(a);
-     b = Number(b);
+//      a = Number(a);
+//      b = Number(b);
    
-     // Если а и б не число и не больше 10
-     if (!a && !b) throw new Error();
-     if (a > 10 || b > 10) throw new Error();
+//      // Если а и б не число и не больше 10
+//      if (!a && !b) throw new Error();
+//      if (a > 10 || b > 10) throw new Error();
    
-     // Производим арифметичекую операцию
-     switch (operators) {
-       case '+':
-         sum = a + b;
-         break;
+//      // Производим арифметичекую операцию
+//      switch (operators) {
+//        case '+':
+//          sum = a + b;
+//          break;
    
-       case '-':
-         sum = a - b;
-         break;
+//        case '-':
+//          sum = a - b;
+//          break;
    
-       case '*':
-         sum = a * b;
-         break;
+//        case '*':
+//          sum = a * b;
+//          break;
    
-       case '/':
-         sum = Math.floor(a / b);
-         break;
-     }
+//        case '/':
+//          sum = Math.floor(a / b);
+//          break;
+//      }
    
-     // Проверяем оператор на налачие данных
-     if (sum == undefined) throw new Error();
-     sum = sum.toString();
+//      // Проверяем оператор на налачие данных
+//      if (sum == undefined) throw new Error();
+//      sum = sum.toString();
    
-     // если j > 1 то переводит число в Римскую систему счисления
-     if (j > 1) {
-       if (sum <= 0) {
-         return '';
-       }
-       if (sum <= 10) {
-         sum = romeN[sum - 1];
-       }
-       if (sum > 10 && sum < 40) {
-         sum = sum.split('');
+//      // если j > 1 то переводит число в Римскую систему счисления
+//      if (j > 1) {
+//        if (sum <= 0) {
+//          return '';
+//        }
+//        if (sum <= 10) {
+//          sum = romeN[sum - 1];
+//        }
+//        if (sum > 10 && sum < 40) {
+//          sum = sum.split('');
    
-         k = 'X'.repeat(sum[0]);
+//          k = 'X'.repeat(sum[0]);
    
-         if (sum[sum.length - 1] == 0) {
-           sum = '';
-           sum = k;
-         } else {
-           sum = sum[sum.length - 1];
+//          if (sum[sum.length - 1] == 0) {
+//            sum = '';
+//            sum = k;
+//          } else {
+//            sum = sum[sum.length - 1];
    
-           sum = k + romeN[sum - 1];
-         }
-       }
-       if (sum >= 40 && sum < 50) {
-         sum = sum.split('');
-         k = 'XL';
+//            sum = k + romeN[sum - 1];
+//          }
+//        }
+//        if (sum >= 40 && sum < 50) {
+//          sum = sum.split('');
+//          k = 'XL';
    
-         if (sum[sum.length - 1] == 0) {
-           sum = '';
-           sum = k;
-         } else {
-           sum = sum[sum.length - 1];
+//          if (sum[sum.length - 1] == 0) {
+//            sum = '';
+//            sum = k;
+//          } else {
+//            sum = sum[sum.length - 1];
    
-           sum = k + romeN[sum - 1];
-         }
-       }
-       if (sum >= 50 && sum < 90) {
-         sum = sum.split('');
-         k = 'L' + 'X'.repeat(sum[0] - 5);
+//            sum = k + romeN[sum - 1];
+//          }
+//        }
+//        if (sum >= 50 && sum < 90) {
+//          sum = sum.split('');
+//          k = 'L' + 'X'.repeat(sum[0] - 5);
    
-         if (sum[sum.length - 1] == 0) {
-           sum = '';
-           sum = k;
-         } else {
-           sum = sum[sum.length - 1];
+//          if (sum[sum.length - 1] == 0) {
+//            sum = '';
+//            sum = k;
+//          } else {
+//            sum = sum[sum.length - 1];
    
-           sum = k + romeN[sum - 1];
-         }
-       }
-       if (sum >= 90 && sum <= 100) {
-         sum = sum.split('');
-         k = 'XC';
-         if (sum.length > 2) sum = 'C';
-         else if (sum[sum.length - 1] == 0) {
-           sum = '';
-           sum = k;
-         } else {
-           sum = sum[sum.length - 1];
+//            sum = k + romeN[sum - 1];
+//          }
+//        }
+//        if (sum >= 90 && sum <= 100) {
+//          sum = sum.split('');
+//          k = 'XC';
+//          if (sum.length > 2) sum = 'C';
+//          else if (sum[sum.length - 1] == 0) {
+//            sum = '';
+//            sum = k;
+//          } else {
+//            sum = sum[sum.length - 1];
    
-           sum = k + romeN[sum - 1];
-         }
-       }
+//            sum = k + romeN[sum - 1];
+//          }
+//        }
    
-       return sum;
-     } // Если j < 0 то Латинское система счисления
-     else {
-       return sum;
-     }
-   }
+//        return sum;
+//      } // Если j < 0 то Латинское система счисления
+//      else {
+//        return sum;
+//      }
+//    }
 
-   calculator('1 + 1')
+//    calculator('1 + 1')
 
 
    /// -------------------- ///
-   function calculator(str) {
-     //Convert
-     let a,
-       b,
-       sum,
-       k,
-       romeN = ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX', 'X', ''],
-       operators = ['+', '-', '*', '/'],
-       j = 0;
+
+//    function calculator(str) {
+//      //Convert
+//      let a,
+//        b,
+//        sum,
+//        k,
+//        romeN = ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX', 'X'],
+//        operators = ['+', '-', '*', '/'],
+//        j = 0;
    
-     str = str.split(' ');
+//      str = str.split(' ');
    
-     // Проверяем на ошибки
-     if (str.length == 1) throw new Error();
-     if (str.length > 3) throw new Error();
+//      // Проверяем на ошибки
+//      if (str.length == 1) throw new Error();
+//      if (str.length > 3) throw new Error();
    
-     //Определяем опрератор
-     operators.map((num) => {
-       if (num == str[1]) operators = num;
-     });
+//      //Определяем опрератор
+//      operators.map((num) => {
+//        if (num == str[1]) operators = num;
+//      });
    
-     b = str[str.length - 1];
-     a = str[0];
+//      b = str[str.length - 1];
+//      a = str[0];
    
-     // Проверяем на похожие Системы счисления
-     if (
-       (!romeN.includes(a) && romeN.includes(b))
-     ) {
-       throw new Error();
-     }
+//      // Проверяем на похожие Системы счисления
+//      if ((romeN.includes(a) && !romeN.includes(b))) {
+//        throw new Error();
+//      }
    
-     // Конвертируем Римские числа в Латинскую систему счисления
-     romeN.map((num, i) => {
-       if (a == num) {
-         a = i + 1;
-         j++;
-       }
+//      // Конвертируем Римские числа в Латинскую систему счисления
+//      romeN.map((num, i) => {
+//        if (a == num) {
+//          a = i + 1;
+//          j++;
+//        }
    
-       if (b == num) {
-         b = i + 1;
-         j++;
-       }
-     });
+//        if (b == num) {
+//          b = i + 1;
+//          j++;
+//        }
+//      });
    
-     a = Number(a);
-     b = Number(b);
+//      a = Number(a);
+//      b = Number(b);
    
-     // Если а и б не число и не больше 10
-     if (!a || !b) throw new Error();
-     if (a > 10 || b > 10) throw new Error();
+//      // Если а и б не число и не больше 10
+//      if (!a || !b) throw new Error();
+//      if (a > 10 || b > 10) throw new Error();
    
-     // Производим арифметичекую операцию
-     switch (operators) {
-       case '+':
-         sum = a + b;
-         break;
+//      // Производим арифметичекую операцию
+//      switch (operators) {
+//        case '+':
+//          sum = a + b;
+//          break;
    
-       case '-':
-         sum = a - b;
-         break;
+//        case '-':
+//          sum = a - b;
+//          break;
    
-       case '*':
-         sum = a * b;
-         break;
+//        case '*':
+//          sum = a * b;
+//          break;
    
-       case '/':
-         sum = Math.floor(a / b);
-         break;
-     }
+//        case '/':
+//          sum = Math.floor(a / b);
+//          break;
+//      }
    
-     // Проверяем оператор на налачие данных
-     if (sum == undefined) throw new Error();
-     sum = sum.toString();
+//      // Проверяем оператор на налачие данных
+//      if (sum == undefined) throw new Error();
+//      sum = sum.toString();
+     
+//      // если j > 1 то переводит число в Римскую систему счисления
+//      if (j > 1) {
+//        if (sum <= 0) {
+//          return '';
+//        }
+//        if (sum <= 10) {
+//          sum = romeN[sum - 1];
+//        }
+//        if (sum > 10 && sum < 40) {
+//          sum = sum.split('');
    
-     // если j > 1 то переводит число в Римскую систему счисления
-     if (j > 1) {
-       if (sum <= 0) {
-         return '';
-       }
-       if (sum <= 10) {
-         sum = romeN[sum - 1];
-       }
-       if (sum > 10 && sum < 40) {
-         sum = sum.split('');
+//          k = 'X'.repeat(sum[0]);
    
-         k = 'X'.repeat(sum[0]);
+//          if (sum[sum.length - 1] == 0) {
+//            sum = '';
+//            sum = k;
+//          } else {
+//            sum = sum[sum.length - 1];
    
-         if (sum[sum.length - 1] == 0) {
-           sum = '';
-           sum = k;
-         } else {
-           sum = sum[sum.length - 1];
+//            sum = k + romeN[sum - 1];
+//          }
+//        }
+//        if (sum >= 40 && sum < 50) {
+//          sum = sum.split('');
+//          k = 'XL';
    
-           sum = k + romeN[sum - 1];
-         }
-       }
-       if (sum >= 40 && sum < 50) {
-         sum = sum.split('');
-         k = 'XL';
+//          if (sum[sum.length - 1] == 0) {
+//            sum = '';
+//            sum = k;
+//          } else {
+//            sum = sum[sum.length - 1];
    
-         if (sum[sum.length - 1] == 0) {
-           sum = '';
-           sum = k;
-         } else {
-           sum = sum[sum.length - 1];
+//            sum = k + romeN[sum - 1];
+//          }
+//        }
+//        if (sum >= 50 && sum < 90) {
+//          sum = sum.split('');
+//          k = 'L' + 'X'.repeat(sum[0] - 5);
    
-           sum = k + romeN[sum - 1];
-         }
-       }
-       if (sum >= 50 && sum < 90) {
-         sum = sum.split('');
-         k = 'L' + 'X'.repeat(sum[0] - 5);
+//          if (sum[sum.length - 1] == 0) {
+//            sum = '';
+//            sum = k;
+//          } else {
+//            sum = sum[sum.length - 1];
    
-         if (sum[sum.length - 1] == 0) {
-           sum = '';
-           sum = k;
-         } else {
-           sum = sum[sum.length - 1];
+//            sum = k + romeN[sum - 1];
+//          }
+//        }
+//        if (sum >= 90 && sum <= 100) {
+//          sum = sum.split('');
+//          k = 'XC';
+//          if (sum.length > 2) sum = 'C';
+//          else if (sum[sum.length - 1] == 0) {
+//            sum = '';
+//            sum = k;
+//          } else {
+//            sum = sum[sum.length - 1];
    
-           sum = k + romeN[sum - 1];
-         }
-       }
-       if (sum >= 90 && sum <= 100) {
-         sum = sum.split('');
-         k = 'XC';
-         if (sum.length > 2) sum = 'C';
-         else if (sum[sum.length - 1] == 0) {
-           sum = '';
-           sum = k;
-         } else {
-           sum = sum[sum.length - 1];
+//            sum = k + romeN[sum - 1];
+//          }
+//        }
    
-           sum = k + romeN[sum - 1];
-         }
-       }
+//        return sum;
+//      } 
+//      // Если j < 0 то Латинское система счисления
+//      else {
+//        return sum;
+//      }
+//    }
    
-       return sum;
-     } // Если j < 0 то Латинское система счисления
-     else {
-       return sum;
-     }
-   }
+// console.log(calculator('1 + V'));
+
+
+// -------------- CALC ---------------- //
+
+function calculator(str) { 
+    //Convert 
+    let a, 
+      b, 
+      sum, 
+      k, 
+      romeN = ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX', 'X', ''], 
+      operators = ['+', '-', '*', '/'], 
+      j = 0; 
    
-    calculator('4 - 12');
+    str = str.split(' '); 
+   
+    // Проверяем на ошибки 
+    if (str.length == 1) throw new Error(); 
+    if (str.length > 3) throw new Error(); 
+   
+    ///console.log(str);
+    //Определяем опрератор 
+    operators.map((num) => { 
+      if (num == str[1]) operators = num; 
+      //console.log(operators);
+    }); 
+   
+    b = str[str.length - 1]; 
+    a = str[0]; 
+   
+     //console.log(b);
+     //console.log(a);
+
+    // Проверяем на похожие Системы счисления 
+    if ( ( romeN.includes(a) && !romeN.includes(b) ) || ( !romeN.includes(a) && romeN.includes(b) )) { 
+      throw new Error(); 
+    } 
+   
+    // Конвертируем Римские числа в Латинскую систему счисления 
+    romeN.map((num, i) => { 
+      if (a == num) { 
+        a = i + 1; 
+        j++; 
+      } 
+   
+      if (b == num) { 
+        b = i + 1; 
+        j++; 
+      } 
+    }); 
+   
+    a = Number(a); 
+    b = Number(b);
+    
+    // console.log(a);
+    // console.log(b);
+   
+    // Если а и б не число и не больше 10 
+    if (!a || !b) throw new Error(); 
+    if (a > 10 || b > 10) throw new Error(); 
+   
+    // Производим арифметичекую операцию 
+    switch (operators) { 
+      case '+': 
+        sum = a + b; 
+        break; 
+   
+      case '-': 
+        sum = a - b; 
+        break; 
+   
+      case '*': 
+        sum = a * b; 
+        break; 
+   
+      case '/': 
+        sum = Math.floor(a / b); 
+        break; 
+    } 
+   
+    // Проверяем оператор на налачие данных 
+    if (sum == undefined) throw new Error(); 
+    sum = sum.toString(); 
+   
+    // если j > 1 то переводит число в Римскую систему счисления 
+    if (j > 1) { 
+      if (sum <= 0) { 
+        return ''; 
+      } 
+      if (sum <= 10) { 
+        sum = romeN[sum - 1]; 
+      } 
+      if (sum > 10 && sum < 40) { 
+        sum = sum.split(''); 
+   
+        k = 'X'.repeat(sum[0]); 
+   
+        if (sum[sum.length - 1] == 0) { 
+          sum = ''; 
+          sum = k; 
+        } else { 
+          sum = sum[sum.length - 1]; 
+   
+          sum = k + romeN[sum - 1]; 
+        } 
+      } 
+      if (sum >= 40 && sum < 50) { 
+        sum = sum.split(''); 
+        k = 'XL'; 
+   
+        if (sum[sum.length - 1] == 0) { 
+          sum = ''; 
+          sum = k; 
+        } else { 
+          sum = sum[sum.length - 1]; 
+   
+          sum = k + romeN[sum - 1]; 
+        } 
+      } 
+      if (sum >= 50 && sum < 90) { 
+        sum = sum.split(''); 
+        k = 'L' + 'X'.repeat(sum[0] - 5); 
+   
+        if (sum[sum.length - 1] == 0) { 
+          sum = ''; 
+          sum = k; 
+        } else { 
+          sum = sum[sum.length - 1]; 
+   
+          sum = k + romeN[sum - 1]; 
+        } 
+      } 
+      if (sum >= 90 && sum <= 100) { 
+        sum = sum.split(''); 
+        k = 'XC'; 
+        if (sum.length > 2) sum = 'C'; 
+        else if (sum[sum.length - 1] == 0) { 
+          sum = ''; 
+          sum = k; 
+        } else { 
+          sum = sum[sum.length - 1]; 
+   
+          sum = k + romeN[sum - 1]; 
+        } 
+      } 
+   
+      return sum; 
+    } // Если j < 0 то Латинское система счисления 
+    else { 
+      return sum; 
+    } 
+  }
+
+calculator('1 + 1');
+
+// ----------------------------------- //
